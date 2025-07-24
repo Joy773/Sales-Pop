5b863e4fa18db1be32e53ceb633f4883b2149a61
Moinul Islam <web.moinul@gmail.com>
2025-07-24 08:59:07 +0600

    staging testing

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -106,7 +106,7 @@ export default function Index() {
 
   return (
     <Page>
-      <TitleBar title="Dashboard">
+      <TitleBar title="Staging">
         <button variant="primary" onClick={generateProduct}>
           Generate a product
         </button>
